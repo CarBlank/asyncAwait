8ecf39afda3edebf1b317f379d4b4c75ea580d12
CarBlank <crblanquer@gmail.com>
2024-05-22 18:45:11 +0200

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+const API_URL = 'https://rickandmortyapi.com/api/character'
+const charactersContainer = document.querySelector('#characters')
+const formSearch = document.getElementById('form')
+const searchInput = document.getElementById('searchId')
